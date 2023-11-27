94c4d45fb623cceae733c0f775302b06baf4ed07
Vincent10z <vincentsmithray@gmail.com>
2023-11-27 18:10:21 +0200

    Populate main functions

--- script.js
+++ script.js
@@ -34,12 +34,15 @@ function getPlayerChoice(){
 
     let playerSelection = prompt("Please select your choice of Rock, Paper, or Scissor: ");
 
-    if(playerSelection === Null){
-        
-    }
-
     playerSelection = playerSelection.toLowerCase()
 
+
+    // if(playerSelection !== 'rock' || playerSelection !== 'paper' || playerSelection !== 'scissor'){
+    //     console.log("Please Enter a valid selection");
+    //     return 1;
+    // }
+
+    
     console.log(playerSelection);
 
 
@@ -49,9 +52,51 @@ function getPlayerChoice(){
 
 function playRound(playerSelection, computerSelection){
 
+    let whoWon = '';
+
+    if(playerSelection === computerSelection)
+    {
+        whoWon = "You Tie!";
+        return whoWon;
+    }
+    else if(playerSelection === 'rock'){
+        if(computerSelection === 'paper'){
+            whoWon = "You Lose! Paper beats Rock";
+            return whoWon;
+        }
+        if(computerSelection === 'scissor'){
+            whoWon = "You Win! Rock beats Scissor";
+            return whoWon;
+        }
+    }
+    else if(playerSelection === 'paper'){
+
+        if(computerSelection === 'rock'){
+            whoWon = "You Win! Paper beats Rock";
+            return whoWon;
+        }
+        if(computerSelection === 'scissor'){
+            whoWon = "You Lose! Scissor beats Paper";
+            return whoWon;
+        }
+    }
+    else if(playerSelection === 'scissor'){
+
+        if(computerSelection === 'rock'){
+            whoWon = "You Lose! Rock beats Scissor";
+            return whoWon;
+        }
+        if(computerSelection === 'paper'){
+            whoWon = "You Win! Scissor beats Paper";
+            return whoWon;
+        }
+    }
+  
 
 
 }
 
 let computerSelection = getComputerChoice();
 let playerSelection = getPlayerChoice();
+let whoWon = playRound(playerSelection, computerSelection);
+console.log(whoWon);
